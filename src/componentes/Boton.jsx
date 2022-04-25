b3eb9d59bb92c3654ec7f0e025070851132ef9a6
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-04-25 08:36:20 -0400

    install

---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Boton = () => {
+    //Aquí va la lógica
+    
+  return (
+    <div className='content-Button'>
+        {/* prueba */}
+       <a href="https://www.google.cl"> <button>Mesa</button> </a> 
+       <a href="https://www.laboratoria.la"> <button>Cocina</button> </a> 
+    </div>
+  )
+}
+
+export default Boton
