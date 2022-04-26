9513907245b27a81f00f1ef947fd8c62c95fec9a
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-04-26 11:00:56 -0400

    Rutas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './componentes/Home';
-import {BrowseRouter, link} from 'react-router-dom'
+import Mesa from './componentes/Mesa';
+import Cocina from './componentes/Cocina';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
 function App() {
   return (
-    
-    <Home/>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/' element={<Mesa/>} />
+    <Route path='/' element={<Cocina/>} />
+    </Routes>
+    </BrowserRouter>
     
   );
 }
