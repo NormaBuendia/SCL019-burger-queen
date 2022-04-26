06430804c9b805713f1cada0ce7657cde8044a68
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-04-26 11:13:44 -0400

    Add notFound

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './componentes/Home';
 import Mesa from './componentes/Mesa';
 import Cocina from './componentes/Cocina';
+import NotFound from './componentes/NotFound';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
@@ -11,8 +12,9 @@ function App() {
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home/>} />
-    <Route path='/' element={<Mesa/>} />
-    <Route path='/' element={<Cocina/>} />
+    <Route path='/mesa' element={<Mesa/>} />
+    <Route path='/cocina' element={<Cocina/>} />
+    <Route path='*' element={<NotFound/>} />
     </Routes>
     </BrowserRouter>
     
