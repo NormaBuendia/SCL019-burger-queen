f071acc6f08839981e0d0cf9658ec486cddbc7ab
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-05-10 22:52:18 -0400

    not found

--- src/componentes/NotFound.jsx
+++ src/componentes/NotFound.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import pilonAnimado from '../img-jsx/pilonAnimado.gif'
 
 export default function NotFound(){
     return(
         <div>
-            <h1>404 Not Found</h1>
+            <h1> Not Found </h1>
+            <img src={pilonAnimado} className="pilonAimado" alt="pilon_animado"></img>
         </div>
     )
 }
