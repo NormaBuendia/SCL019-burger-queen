820092b7bcf29f3731a0ca7442f05188e814bdb4
May Ortega Torres <mayorlen.ortega@gmail.com>
2022-05-17 17:20:33 -0400

    changes CSS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Home from "./componentes/views/Home";
 import Mesa from "./componentes/views/Mesa";
 import Cocina from "./componentes/views/Cocina";
 import NotFound from "./componentes/NotFound";
-import Caja from "./componentes/views/Caja";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { UseStates } from "./componentes/useContext/UserContent";
 
@@ -17,7 +16,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/views/Mesa" element={<Mesa />} />
           <Route path="/views/Cocina" element={<Cocina />} />
-          <Route path="/views/Caja" element={<Caja />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- src/componentes/PedidosNuevos.jsx
+++ src/componentes/PedidosNuevos.jsx
@@ -1,12 +1,16 @@
 import React, {useEffect, useContext} from "react"
 import { UserContent } from "./useContext/UserContent";
-import { collection, getDocs, updateDoc, doc} from "firebase/firestore"
+import { addDoc, collection, getDocs, updateDoc, doc, Timestamp, deleteDoc} from "firebase/firestore"
 import {db} from '../firebase-config'
-import Pedidos from "./Pedidos";
+
+
 
 
 export default function PedidosNuevos (){
     const { orden, setOrden } = useContext(UserContent);
+    const { cliente, setCliente } = useContext(UserContent);
+    const { cart, setCart } = useContext(UserContent);
+    
 
     useEffect(() => {
         
@@ -21,25 +25,51 @@ export default function PedidosNuevos (){
         }
 
         getDatos()
-    }, [])
+    }, [setOrden])
 
     const changeStatus = async (id) => {
         const orderDoc = doc(db, "Pedidos", id);
-        const newStatus= { status: "Listo" };
+        const newStatus= { status: "Listo"}
         await updateDoc(orderDoc, newStatus);
+        window.location.reload(false);
+        
       };
 
-      const archivar = async (id) => {
-        const orderData = doc(db, "Pedidos", id);
-        const newCollection= { collection: "Archivo" };
-        await updateDoc(orderData, newCollection);
+      const sendToCaja = async () => {
+        
+        try {
+          const docRef = await addDoc(collection(db, "Caja"), {
+            Pedido: orden,
+            dateOrder: Timestamp.fromDate(new Date()),
+            
+          });
+         alert('Pedido se enviará a caja')
+         window.location.reload(false)
+         console.log(docRef);
+        } catch (e) {
+          console.log("error", e);
+        }
+        setCliente("");
+        setOrden([]);
+        
       };
+    
 
-
+      const eliminar = async (id) => {
+        try {
+          await deleteDoc(doc(db, "Pedidos", id));
+          const arrayFiltrado = orden.filter(item => item.id !== id)
+          setOrden(arrayFiltrado)
+          alert('¿Está seguro que desea eliminar el pedido?')
+          window.location.reload(false)
+        } catch (error) {
+          console.log(error)
+        }
+    }
   
   
     
-
+    const itemsPrice = cart.reduce((a, c) => a + c.cant * c.price, 0);
 
 return (
     
@@ -52,14 +82,13 @@ return (
         {
             orden.map(item => (
                 
-               
-                   
                <th key={item.id} className="thPedidos">
                <div className="contentPedidos" >
                       <section className="sectionPedidos1">
+                      <span onClick={()=> eliminar(item.id)} className="x-delete"><strong> X </strong></span>    
                       <span><strong>Cliente: </strong>{item.Cliente}</span>
                       <span><strong>Status: </strong>{item.status}</span>
-                     <span><strong>Hora: </strong>{item.dateOrder.toDate().toLocaleTimeString()}</span>
+                      <span><strong>Hora: </strong>{item.dateOrder.toDate().toLocaleTimeString()}</span>
                       </section>
                       <section className="sectionPedidos2">
                       {
@@ -67,7 +96,7 @@ return (
                           item.Pedido.map(item => (
                              
                             <div className="card text-white bg-warning mb-3" id="cocina2" key={item.id}>
-                             <p className="spanPedido">{item.cant} {item.name}</p>
+                             <p className="spanPedido">{item.cant} {item.name} {item.price}{item.badge}</p>
                              
                         </div>
                         
@@ -75,7 +104,7 @@ return (
                             ))           
                         }
         <button onClick={()=> changeStatus(item.id)} className="btnEntrega" >Entregar</button>
-        <button onClick={()=> archivar(item.id)} className="btnArchivo" >Archivar</button>
+        <button onClick={()=> sendToCaja(item.id)} className="btnArchivo" >Enviar a Caja</button>
            </section>
            </div>
            </th>
