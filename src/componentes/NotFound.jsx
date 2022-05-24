d75d0a4c585a15df2dde21e4b88fa9febd9e50fc
= <mayorlen.ortega@gmail.com>
2022-05-24 01:01:51 -0400

    add link to home

--- src/componentes/NotFound.jsx
+++ src/componentes/NotFound.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import pilonAnimado from '../img-jsx/pilonAnimado.gif'
+import { Link } from 'react-router-dom'
 
 export default function NotFound(){
     return(
         <div>
             <h1> Not Found </h1>
             <img src={pilonAnimado} className="pilonAimado" alt="pilon_animado"></img>
+            <Link to='/'>
+        <button className="btn-404">ir al Home</button>
+           </Link> 
         </div>
     )
 }
